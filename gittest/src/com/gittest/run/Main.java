88a73a783daf419d0c584b017052c71106212631
GDJ <GDJ@DESKTOP-SGUODFS>
2023-10-17 16:51:22 +0900

    bs_dev브랜치제엇 mail 수정

--- gittest/src/com/gittest/run/Main.java
+++ gittest/src/com/gittest/run/Main.java
@@ -3,8 +3,8 @@ package com.gittest.run;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("안녕 git 반가워!");
+		System.out.println("난 DEV브랜치 오홍홍호호호");
 
 	}
 
